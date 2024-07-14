aaafe33be9494feb310f8e07ecbd6bdef57da72d
NewsCorpGSOC <lagsoc@newscorp.com>
2024-07-13 18:40:54 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+const config = {
+  users: {
+    'tporter.consultant@newscorp.com': 'GSOCIntranet2024!',
+    'lagsoc@newscorp.com': 'GSOCIntranet2024'
+  }
+};
+
+export default config;
