7702d7613ed736f787fc49333012e35f0cdb4d4c
NewsCorpGSOC <lagsoc@newscorp.com>
2024-07-25 11:11:10 -0700

    Update rss-feed-collector.js - Converted all Feeds to use Proxy

--- rss-feed-collector.js
+++ rss-feed-collector.js
@@ -76,6 +76,283 @@ document.addEventListener('DOMContentLoaded', () => {
       url: 'https://corsproxy.io/?https%3A%2F%2Fnews.un.org%2Ffeed%2Fsubscribe%2Fen%2Fnews%2Fall%2Frss.xml',
       source: 'UN News'
     },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fnews.yahoo.com%2Frss%2Fworld',
+      source: 'Yahoo News'
+    },
+    //{ --------------------Required "Human" Verification ---------------------
+    //  url: 'https://www.politico.com/rss/politicopicks.xml',
+    //  source: 'Politico'
+    //},
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.politico.eu%2Ffeed%2F',
+      source: 'Politico EU'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.thestar.com%2Fsearch%2F%3Ff%3Drss%26t%3Darticle%26c%3Dnews%2Fworld*%26l%3D50%26s%3Dstart_time%26sd%3Ddesc',
+      source: 'The Star'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.brandonsun.com%2Ffeed%3Fpath%3D%252Fnational%252Fbreaking-news',
+      source: 'Brandon Sun'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.thehindubusinessline.com%2Fnews%2F%3Fservice%3Drss',
+      source: 'The Hindu Business Line'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fzeenews.india.com%2Frss%2Fworld-news.xml',
+      source: 'Zee News India'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.livemint.com%2Frss%2Fpolitics',
+      source: 'Live Mint India'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.ft.com%2Fworld-uk%3Fformat%3Drss',
+      source: 'Financial Times'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Ffeeds.skynews.com%2Ffeeds%2Frss%2Fpolitics.xml',
+      source: 'Sky News Politics'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Ffeeds.skynews.com%2Ffeeds%2Frss%2Fpolitics.xml',
+      source: 'Sky News Politics',
+      backups: [
+        'https://corsproxy.io/?https%3A%2F%2Ffeeds.skynews.com%2Ffeeds%2Frss%2Fworld.xml'
+      ]
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.economist.com%2Feurope%2Frss.xml',
+      source: 'The Economist - Europe'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.economist.com%2Fthe-americas%2Frss.xml',
+      source: 'The Economist - Americas'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.economist.com%2Fmiddle-east-and-africa%2Frss.xml',
+      source: 'The Economist - MENA'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.nzherald.co.nz%2Farc%2Foutboundfeeds%2Frss%2Fsection%2Fworld%2F%3FoutputType%3Dxml%26_website%3Dnzh',
+      source: 'The New Zealand Herald'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Ffeeds.a.dj.com%2Frss%2FRSSWorldNews.xml',
+      source: 'Wall Street Journal'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Frss.jpost.com%2Frss%2Frssfeedsarabisraeliconflict.aspx',
+      source: 'The Jerusalem Post - Arab-Israeli Conflict'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Frss.jpost.com%2Frss%2Frssfeedsheadlines.aspx',
+      source: 'The Jerusalem Post - Breaking News'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Frss.jpost.com%2Frss%2Frssfeedsinternational',
+      source: 'The Jerusalem Post - World News'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Frss.jpost.com%2Frss%2Fisrael-hamas-war',
+      source: 'The Jerusalem Post - Israel-Hamas War'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Frss.jpost.com%2Frss%2Frssfeedsmiddleeastnews.aspx',
+      source: 'The Jerusalem Post - Middle East News',
+      backups: [
+        'https://corsproxy.io/?https%3A%2F%2Frss.jpost.com%2Frss%2Frssfeedsiran'
+      ]
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Frss.jpost.com%2Frss%2Frssukrainerussiawar',
+      source: 'The Jerusalem Post - Ukraine-Russia War'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Frss.jpost.com%2Frss%2Frssfeedsgaza.aspx',
+      source: 'The Jerusalem Post - Gaza'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fthehill.com%2Ffeed%2F%3Ffeed%3Dpartnerfeed-news-feed%26format%3Drss',
+      source: 'The Hill'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fnypost.com%2Ffeed%2F',
+      source: 'New York Post'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fnews.usni.org%2Ffeed',
+      source: 'USNI News'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.israelhayom.com%2Ffeed%2F',
+      source: 'Israel Hayom'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fkyivindependent.com%2Fnews-archive%2Frss%2F',
+      source: 'The Kyiv Independent'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fzobo_egviy',
+      source: 'VOA News - East Asia'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fz-botevtiq',
+      source: 'VOA News - Africa'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fzmjuqteb_kqo',
+      source: 'VOA News - China News'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fz_-mqyerv_qv',
+      source: 'VOA News - South and Central Asia'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fzrbopeuvim',
+      source: 'VOA News - Middle East',
+      backups: [
+        'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fzvgmqieo__qm'
+      ]
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fzjboveytit',
+      source: 'VOA News - Europe'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fzt_rqyeirqqy',
+      source: 'VOA News - Ukraine'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fzoripegtim',
+      source: 'VOA News - Americas'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fzbuiqyetppqv',
+      source: 'VOA News - Extremism Watch'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fziu-qoejibqo',
+      source: 'VOA News - Flashpoint Global Crises'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fz-ukqqevpgqi',
+      source: 'VOA News - Flashpoint Ukraine'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fzumgqoepbgqp',
+      source: 'VOA News - International Edition'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fzvq-qveok-qv',
+      source: 'VOA News - Issues in the News'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.voanews.com%2Fapi%2Fzmbjqvebv-qr',
+      source: 'VOA News - Fact Checks'
+    },
+    {
+      url: 'https://blog.4president.us/2024/atom.xml',
+      source: 'Blog4President',
+      backups: [
+        'https://blog.4president.us/2024/index.rdf',
+        'https://blog.4president.us/2024/rss.xml'
+      ]
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.spaceforce.mil%2FDesktopModules%2FArticleCS%2FRSS.ashx%3FContentType%3D1%26Site%3D1060%26isdashboardselected%3D0%26max%3D20%26Category%3D23812',
+      source: 'Space Force News'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.smh.com.au%2Frss%2Fworld.xml',
+      source: 'Sydney Morning Herald'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.state.gov%2Frss-feed%2Fafrica%2Ffeed%2F',
+      source: 'U.S. State Department - Africa'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.state.gov%2Frss-feed%2Feast-asia-and-the-pacific%2Ffeed%2F',
+      source: 'U.S. State Department - East Asia and the Pacific'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.state.gov%2Frss-feed%2Feurope-and-eurasia%2Ffeed%2F',
+      source: 'U.S. State Department - Europe and Eurasia'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.state.gov%2Frss-feed%2Fnear-east%2Ffeed%2F',
+      source: 'U.S. State Department - Near East'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.state.gov%2Frss-feed%2Fsouth-and-central-asia%2Ffeed%2F',
+      source: 'U.S. State Department - South and Central Asia'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.defense.gov%2FDesktopModules%2FArticleCS%2FRSS.ashx%3FContentType%3D9%26Site%3D945%26max%3D10',
+      source: 'U.S. Department of Defense'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fsearch.cnbc.com%2Frs%2Fsearch%2Fcombinedcms%2Fview.xml%3FpartnerId%3Dwrss01%26id%3D100727362',
+      source: 'CNBC'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.rt.com%2Frss%2Fnews%2F',
+      source: 'RT World News'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.understandingwar.org%2Frss.xml',
+      source: 'ISW',
+      backups: [
+        'https://corsproxy.io/?https%3A%2F%2Fwww.understandingwar.org%2Ffeeds.xml'
+      ]
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.cbc.ca%2Fwebfeed%2Frss%2Frss-world',
+      source: 'CBC'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.thecipherbrief.com%2Ffeed',
+      source: 'Cipher Brief'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.ctvnews.ca%2Frss%2Fworld%2Fctvnews-ca-world-public-rss-1.822289',
+      source: 'CTV News'
+    },
+    {
+      url: 'https://corsproxy.io/?https%3A%2F%2Fwww.channelnewsasia.com%2Fapi%2Fv1%2Frss-outbound-feed%3F_format%3Dxml%26category%3D6511',
+      source: 'Channel News Asia'
+    },
+/*----------------------------SOCIAL MEDIA--------------------------------*/
+    {
+      url: 'https://news.google.com/rss/search?q=site:twitter.com/centcom+when:7d&hl=en-US&gl=US&ceid=US:en',
+      source: 'USCENTCOM - TwitterX'
+    },
+    {
+      url: 'https://news.google.com/rss/search?q=site:twitter.com/middleeasteye+when:7d&hl=en-US&gl=US&ceid=US:en',
+      source: 'Middle East Eye - TwitterX'
+    },
+    {
+      url: 'https://news.google.com/rss/search?q=site:twitter.com/mondefense+when:7d&hl=en-GB&gl=GB&ceid=GB:en',
+      source: 'Taiwan Ministry of National Defense - TwitterX'
+    },
+    {
+      url: 'https://news.google.com/rss/search?q=site:twitter.com/gardaworldc24+when:7d&hl=en-US&gl=US&ceid=US:en',
+      source: 'Crisis24 - TwitterX'
+    },
+    {
+      url: 'https://news.google.com/rss/search?q=site:twitter.com/aggregateosint+when:7d&hl=en-US&gl=US&ceid=US:en',
+      source: 'OSINT Aggregator - TwitterX'
+    },
+    {
+      url: 'https://news.google.com/rss/search?q=site:twitter.com/thestudyofwar+when:7d&hl=en-US&gl=US&ceid=US:en',
+      source: 'Institute for the Study of War - TwitterX'
+    },
+    {
+      url: 'https://news.google.com/rss/search?q=site:twitter.com/sentdefender+when:7d&hl=en-US&gl=US&ceid=US:en',
+      source: 'OSINT Defender - TwitterX'
+    },
   ];
   
   rssFeeds.sort((a, b) => a.source.localeCompare(b.source));
@@ -200,13 +477,19 @@ document.addEventListener('DOMContentLoaded', () => {
     let adjustedDate = new Date(date);
 
     // Prioritize source-specific adjustments
-    if (source === 'The New York Times') {
+    if (source === 'The Kyiv Independent') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'The Hill') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'The New York Times') {
       adjustedDate.setHours(adjustedDate.getHours() - 0);
     } else if (source === 'BBC News') {
       adjustedDate.setHours(adjustedDate.getHours() - 0);
     } else if (source === 'The Guardian') {
       adjustedDate.setHours(adjustedDate.getHours() - 0);
     } else if (source === 'Al Jazeera - Latest') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'Al Jazeera - World') {
       adjustedDate.setHours(adjustedDate.getHours() - 7);
     } else if (source === 'World Online') {
       adjustedDate.setHours(adjustedDate.getHours() - 7);
@@ -219,6 +502,132 @@ document.addEventListener('DOMContentLoaded', () => {
     } else if (source === 'NPR News') {
       adjustedDate.setHours(adjustedDate.getHours() - 0);
     } else if (source === 'UN News') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'Yahoo News') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'Politico EU') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'Politico') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'The Star') {
+      adjustedDate.setHours(adjustedDate.getHours() - 3);
+    } else if (source === 'Brandon Sun') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'The Hindu Business Line') {
+      adjustedDate.setHours(adjustedDate.getHours() + 0.5);
+    } else if (source === 'Zee News India') {
+      adjustedDate.setHours(adjustedDate.getHours() - 12.5);
+    } else if (source === 'Live Mint India') {
+      adjustedDate.setHours(adjustedDate.getHours() - 6.5);
+    } else if (source === 'Financial Times') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'Channel 4 News') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'Sky News Politics') {
+      adjustedDate.setHours(adjustedDate.getHours() - 8);
+    } else if (source === 'The Economist - Europe') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'The Economist - Americas') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'The Economist - MENA') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'The New Zealand Herald') {
+      adjustedDate.setHours(adjustedDate.getHours() - 3);
+    } else if (source === 'Wall Street Journal') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'The Jerusalem Post - Arab-Israeli Conflict') {
+      adjustedDate.setHours(adjustedDate.getHours() + 3);
+    } else if (source === 'The Jerusalem Post - Breaking News') {
+      adjustedDate.setHours(adjustedDate.getHours() + 3);
+    } else if (source === 'The Jerusalem Post - World News') {
+      adjustedDate.setHours(adjustedDate.getHours() + 3);
+    } else if (source === 'The Jerusalem Post - Israel-Hamas War') {
+      adjustedDate.setHours(adjustedDate.getHours() + 3);
+    } else if (source === 'The Jerusalem Post - Middle East News') {
+      adjustedDate.setHours(adjustedDate.getHours() + 3);
+    } else if (source === 'The Jerusalem Post - Ukraine-Russia War') {
+      adjustedDate.setHours(adjustedDate.getHours() + 3);
+    } else if (source === 'The Jerusalem Post - Gaza') {
+      adjustedDate.setHours(adjustedDate.getHours() + 3);
+    } else if (source === 'New York Post') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'USNI News') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'Israel Hayom') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'VOA News - East Asia') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'VOA News - China News') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'VOA News - Africa') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'VOA News - Middle East') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'VOA News - Europe') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'VOA News - Ukraine') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'VOA News - Americas') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'VOA News - Extremism Watch') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'VOA News - Flashpoint Global Crises') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'VOA News - International Edition') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'VOA News - South and Central Asia') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'VOA News - Issues in the News') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'VOA News - Flashpoint Ukraine') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'VOA News - Fact Checks') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'Blog4President') {
+      adjustedDate.setHours(adjustedDate.getHours() - 2);
+    } else if (source === 'Space Force News') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'Sydney Morning Herald') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'U.S. State Department - Africa') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'U.S. State Department - East Asia and the Pacific') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'U.S. State Department - Europe and Eurasia') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'U.S. State Department - Near East') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'U.S. State Department - South and Central Asia') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'U.S. Department of Defense') {
+      adjustedDate.setHours(adjustedDate.getHours() - 2);
+    } else if (source === 'CNBC') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'RT World News') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'ISW') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'CBC') {
+      adjustedDate.setHours(adjustedDate.getHours() - 3);
+    } else if (source === 'Cipher Brief') {
+      adjustedDate.setHours(adjustedDate.getHours() - 7);
+    } else if (source === 'USCENTCOM - TwitterX') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'Middle East Eye - TwitterX') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'Taiwan Ministry of National Defense - TwitterX') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'Crisis24 - TwitterX') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'OSINT Aggregator - TwitterX') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'Institute for the Study of War - TwitterX') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'OSINT Defender - TwitterX') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'CTV News') {
+      adjustedDate.setHours(adjustedDate.getHours() - 0);
+    } else if (source === 'Channel News Asia') {
       adjustedDate.setHours(adjustedDate.getHours() - 0);
     } else {
       console.warn(`No specific time adjustment found for source: ${source}`);
