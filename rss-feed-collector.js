4a2e7bd79483762229c2b8a0c2b57af47a6d7869
NewsCorpGSOC <lagsoc@newscorp.com>
2024-07-28 15:55:32 -0700

    Update rss-feed-collector.js

--- rss-feed-collector.js
+++ rss-feed-collector.js
@@ -120,7 +120,6 @@ document.addEventListener('DOMContentLoaded', () => {
   async function fetchCSVFiles() {
     const csvFiles = [
       'Israel_Security_Cabinet_News.csv',
-      'National_Weather_Service.csv',
       'Stand_With_Us_Breaking_News.csv',
       'Ukraine_Air_Defense.csv',
       'WOLPalestine.csv'
