d96484365b1baa30254fe6f45c86d91c41196cc0
NewsCorpGSOC <lagsoc@newscorp.com>
2024-08-11 13:02:49 -0700

    Update config.js - Fixed Syntax error

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const config = {
   users: {
     'tporter.consultant@newscorp.com': 'GSOCIntranet2024!',
-    'lagsoc@newscorp.com': 'GSOCIntranet2024'
+    'lagsoc@newscorp.com': 'GSOCIntranet2024',
     'jmagallanes.consultant@newscorp.com': 'highschoolmusical3'
   }
 };
