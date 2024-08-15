6769805068cc51cced5674163c805ae7fbcbbf2a
NewsCorpGSOC <lagsoc@newscorp.com>
2024-08-15 14:18:57 -0700

    Create configDNC.js

---
+++ configDNC.js
@@ -0,0 +1,10 @@
+// config.js
+const configdnc = {
+  users: {
+    'brady.webb@dowjones.com': 'BradyDNC2024!',
+    'lagsoc@newscorp.com': 'GSOCIntranet2024',
+    'jmagallanes.consultant@newscorp.com': 'highschoolmusical3'
+  }
+};
+
+export default configdnc;
