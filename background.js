4fc41c286158025881fb9bd0d2dc4d167a77be44
NewsCorpGSOC <lagsoc@newscorp.com>
2024-10-21 20:16:30 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+jQuery(document).ready(function() {
     // Dropdown hover handling
     const handleDropdownHover = function() {
         if (window.innerWidth > 768) {
