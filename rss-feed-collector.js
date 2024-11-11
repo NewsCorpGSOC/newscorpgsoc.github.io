8434a8b160eea5b8aa3f6b3f41b2d828f4e8cee1
NewsCorpGSOC <lagsoc@newscorp.com>
2024-11-11 11:08:26 -0800

    Update rss-feed-collector.js

--- rss-feed-collector.js
+++ rss-feed-collector.js
@@ -65,7 +65,7 @@ const topicKeywords = {
   },
   'U.S. 2024 Election & Transition': {
     keywords: ['trump', 'vance', 'january 6', 'U.S. election unrest', 'election violence', 'election disruptions', 'election ballot box fires', 'election polling place violence', 'election protest clashes','election security threats', 'election extremist activities', 'election law enforcement response', 'election unrest in [State/City Name]', 'election violence in swing states', 'election disruptions in [State/City Name]', 'election aftermath violence', 'election protest analysis', 'election security assessment', 'election voter concerns about violence', 'election political violence fears', 'election public safety concerns', 'election social media disinformation', 'election online threats', 'election conspiracy theories', 'election National Guard deployment', 'election emergency response', 'election state emergency declarations', 'election voter intimidation', 'election poll worker threats', 'election safety measures'],
-    background: '#3a88aa',
+    background: '#697d85',
     soundFile: 'sounds/news-alert-notification.mp3'
   },
 };
